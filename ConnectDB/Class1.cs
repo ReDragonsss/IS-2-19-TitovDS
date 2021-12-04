3f644cd7f97c8ad60697399bacf8d18d8c9e16e7
Дмитрия Титов <dimaninza@mail.ru>
2021-12-04 17:56:29 +0500

    Тест

---
+++ ConnectDB/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectDB
+{
+    public class ConnectDB
+    {
+
+        public string Connstring = "server=caseum.ru;port=33333;user=test_user;database=db_test;password=test_pass;"; // измененная конструкция из 2 задания
+        public string GetDVozrah()
+        {
+            return Connstring;
+        }
+
+    }
+}
